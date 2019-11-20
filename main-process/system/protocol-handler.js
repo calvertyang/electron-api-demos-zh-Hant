c39f2f3bf58c3e3bc915450b9403e510663516db
Calvert <yz3210@gmail.com>
2019-11-20 15:41:59 +0800

    fix: unpkgd protocol handling on windows

--- main-process/system/protocol-handler.js
+++ main-process/system/protocol-handler.js
@@ -1,6 +1,12 @@
 const {app, dialog} = require('electron')
 
-app.setAsDefaultProtocolClient('electron-api-demos')
+if (process.defaultApp) {
+  if (process.argv.length >= 2) {
+    app.setAsDefaultProtocolClient('electron-api-demos', process.execPath, [path.resolve(process.argv[1])])
+  }
+} else {
+  app.setAsDefaultProtocolClient('electron-api-demos')
+}
 
 app.on('open-url', (event, url) => {
   dialog.showErrorBox('歡迎回來', `你來自：${url}`)
